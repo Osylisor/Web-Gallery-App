cf6411bf4ded79e6a45a646f846d7c45adca4f5d
Osylisor <93229146+Osylisor@users.noreply.github.com>
2022-08-20 01:26:27 +0200

    Delete Web Gallery App directory

--- Web Gallery App/App/static/index.js
+++
@@ -1,8 +0,0 @@
-const toggleButton = document.getElementsByClassName('hamburger-menu')[0];
-const navbarLinks = document.getElementsByClassName('nav-item')[0]
-
-
-toggleButton.addEventListener('click', () =>{
-    navbarLinks.classList.toggle('active');
-    console.log(navbarLinks)
-})
--- Web Gallery App/app.py
+++
@@ -1,9 +0,0 @@
-from App import create_app
-
-
-app = create_app()
-
-
-
-if(__name__ == "__main__"):
-    app.run()
